1bb285bd3720f5465ec9405d28543ddc92cafac2
Nathan Cohen <manathane.co@gmail.com>
2020-02-17 17:04:53 +0100

    bdd

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require('express')
 const path = require('path')
+const {MongoClient} = require('mongodb');
+
 var port = process.env.PORT || 5005;
 let app = express()
 let server = require('http').createServer(app);
 app.use(express.static(__dirname + '/'));
+
+//////////////BASE DE DONNEE////////////////
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://<dbuser>:<dbpassword>@ds131041.mlab.com:31041/heroku_tzdttpft", function (err, db) {
+   
+    db.collection('Persons', function (err, collection) {
+        
+        collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
+        collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
+        collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
+        
+        
+
+        db.collection('Persons').count(function (err, count) {
+            if (err) throw err;
+            
+            console.log('Total Rows: ' + count);
+        });
+    });
+                
+});
 
 //////////////// ROUTES //////////////
 app.get('/', function(req, res) {
