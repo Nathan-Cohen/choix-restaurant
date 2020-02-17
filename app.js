ccdd78ec42cbfaa0c151fb2af3fb7b5b75519a56
Nathan Cohen <manathane.co@gmail.com>
2020-02-17 18:03:58 +0100

    bdd

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+var MongoClient = require('mongodb').MongoClient;
 
 var port = process.env.PORT || 5005;
 let app = express()
@@ -8,30 +9,24 @@ app.use(express.static(__dirname + '/'));
 
 //////////////BASE DE DONNEE////////////////
 
-//lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
+// Connect to the db
+MongoClient.connect("mongodb://heroku_tzdttpft:vinil75020@ds131041.mlab.com:31041/heroku_tzdttpft", function (err, db) {
+   
+    db.collection('Persons', function (err, collection) {
+        
+        collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
+        collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
+        collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
+        
+        
 
-//We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
-
-// Connection URL. This is where your mongodb server is running.
-
-//(Focus on This Variable)
-var url = 'mongodb://heroku_tzdttpft:vinil75020@ds131041.mlab.com:31041/heroku_tzdttpft';      
-//(Focus on This Variable)
-
-// Use connect method to connect to the Server
-  MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
-
-    // do some work here with the database.
-
-    //Close connection
-    db.close();
-  }
+        db.collection('Persons').count(function (err, count) {
+            if (err) throw err;
+            
+            console.log('Total Rows: ' + count);
+        });
+    });
+                
 });
 
 //////////////// ROUTES //////////////
