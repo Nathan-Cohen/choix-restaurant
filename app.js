6b4c1a728acf823876dba7430b1881220ca245b4
Nathan Cohen <manathane.co@gmail.com>
2020-02-17 17:26:55 +0100

    bdd

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
-var MongoClient = require('mongodb').MongoClient;
+// var MongoClient = require('mongodb').MongoClient;
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5005;
 let app = express()
 let server = require('http').createServer(app);
 app.use(express.static(__dirname + '/'));
@@ -10,24 +10,24 @@ app.use(express.static(__dirname + '/'));
 //////////////BASE DE DONNEE////////////////
 
 // Connect to the db
-MongoClient.connect("mongodb://heroku_tzdttpft:<vinil75020>@ds131041.mlab.com:31041/heroku_tzdttpft", function (err, db) {
+// MongoClient.connect("mongodb://heroku_tzdttpft:<vinil75020>@ds131041.mlab.com:31041/heroku_tzdttpft", function (err, db) {
    
-    db.collection('Persons', function (err, collection) {
+//     db.collection('Persons', function (err, collection) {
         
-        collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
-        collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
-        collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
+//         collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
+//         collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
+//         collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
         
         
 
-        db.collection('Persons').count(function (err, count) {
-            if (err) throw err;
+//         db.collection('Persons').count(function (err, count) {
+//             if (err) throw err;
             
-            console.log('Total Rows: ' + count);
-        });
-    });
+//             console.log('Total Rows: ' + count);
+//         });
+//     });
                 
-});
+// });
 
 //////////////// ROUTES //////////////
 app.get('/', function(req, res) {
