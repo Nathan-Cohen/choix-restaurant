e36123035870e1fb72bbb4b9d45c2a829c74bf61
Nathan Cohen <manathane.co@gmail.com>
2020-02-18 17:02:25 +0100

    bdd insert

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'random.html'));
 
     // Use connect method to connect to the Server
-      MongoClient.connect(url, {useNewUrlParser: true}, function (err, db) {
+      MongoClient.connect(url, function (err, db) {
       if (err) {
         console.log('Unable to connect to the mongoDB server. Error:', err);
       } else {
         console.log('Connection established to', url);
         var myobj = { name: "Company Inc", address: "Highway 37" };
-        MongoClient.collection("Persons").insertOne(myobj, function(err, res) {
+          db.collection("Persons").insertOne(myobj, function(err, res) {
           if (err) throw err;
           console.log("1 document inserted");
           db.close();
